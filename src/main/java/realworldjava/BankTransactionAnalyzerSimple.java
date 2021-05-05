c1d6eee6c13d1f42b8974b57054157bdd0ebaf47
riyenas0925 <riyenas0925@gmail.com>
2021-05-05 13:30:00 +0900

    feat : 은행 입출금 내역의 총 수입과 총 지출 계산 기능 구현
    * KISS 원칙을 이용해 하나의 클래스로 구현

---
+++ src/main/java/realworldjava/BankTransactionAnalyzerSimple.java
@@ -0,0 +1,29 @@
+package realworldjava;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class BankTransactionAnalyzerSimple {
+    public static final String RESOURCES = "src/main/resources/";
+
+    public static void main(final String[] args) throws IOException {
+        final Path path = Paths.get(RESOURCES + "bank-data-simple.csv");
+        final List<String> lines = Files.readAllLines(path);
+        double total = 0d;
+
+        for(final String line : lines) {
+            final String[] columns = line.split(",");
+            final double amount = Double.parseDouble(columns[1]);
+            total += amount;
+        }
+
+        System.out.println("The total for all transactions is " + total);
+    }
+}
