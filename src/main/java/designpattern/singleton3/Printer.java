ed75c782e10c35f64b6baea274ff0b858e58de91
riyenas0925 <riyenas0925@gmail.com>
2021-05-08 19:59:52 +0900

    디자인 패턴 : 정적 클래스를 사용한 싱글턴 패턴 효과
    * 싱글턴 패턴을 사용하지 않고 정적 메서드로만 이루어진 정적 클래스를 사용하면 싱글턴 패턴과 동일한 효과를 얻을 수 있다.

---
+++ src/main/java/designpattern/singleton3/Printer.java
@@ -0,0 +1,9 @@
+package designpattern.singleton3;
+
+public class Printer {
+    private static int counter = 0;
+    public synchronized static void print(String str) {
+        counter++;
+        System.out.println(str + counter);
+    }
+}
---
+++ src/main/java/designpattern/singleton3/UserThread.java
@@ -0,0 +1,11 @@
+package designpattern.singleton3;
+
+public class UserThread extends Thread{
+    public UserThread(String name) {
+        super(name);
+    }
+
+    public void run() {
+        Printer.print(Thread.currentThread().getName() + " print using " + ".");
+    }
+}
