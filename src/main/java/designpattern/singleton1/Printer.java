44d357d1f49381301dc2fe7c8d4a95869382b0a3
riyenas0925 <riyenas0925@gmail.com>
2021-05-08 19:42:31 +0900

    디자인 패턴 : 단일스레드의 싱글턴 패턴

---
+++ src/main/java/designpattern/singleton1/Printer.java
@@ -0,0 +1,20 @@
+package designpattern.singleton1;
+
+public class Printer {
+    private static Printer printer = null;
+
+    private Printer() {
+
+    }
+
+    public static Printer getPrinter() {
+        if(printer == null) {
+            printer = new Printer();
+        }
+        return printer;
+    }
+
+    public void print(String str) {
+        System.out.println(str);
+    }
+}
