409c9d1ebeb615a8c51626a6758eaa07348b141b
riyenas0925 <riyenas0925@gmail.com>
2021-05-10 18:48:27 +0900

    디자인 패턴 : 간단한 형광등 모델링

---
+++ src/main/java/designpattern/state/Light.java
@@ -0,0 +1,29 @@
+package designpattern.state;
+
+public class Light {
+    private static int ON = 0;
+    private static int OFF = 1;
+    private int state;
+
+    public Light() {
+        state = OFF;
+    }
+
+    public void on_button_pushed() {
+        if (state == ON) {
+            System.out.println("반응 없음");
+        } else {
+            System.out.println("Light On!");
+            state = ON;
+        }
+    }
+
+    public void off_button_pushed() {
+        if(state == OFF) {
+            System.out.println("반응 없음");
+        } else {
+            System.out.println("Light Off!");
+            state = OFF;
+        }
+    }
+}
