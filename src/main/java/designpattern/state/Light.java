8496b5fffd6c05f02372241f339441bf0e9ffe30
riyenas0925 <riyenas0925@gmail.com>
2021-05-10 18:52:10 +0900

    디자인 패턴 : 현재 형광등 모델의 문제점

--- src/main/java/designpattern/state/Light.java
+++ src/main/java/designpattern/state/Light.java
@@ -3,6 +3,7 @@ package designpattern.state;
 public class Light {
     private static int ON = 0;
     private static int OFF = 1;
+    private static int SLEEPING = 2;
     private int state;
 
     public Light() {
@@ -12,6 +13,8 @@ public class Light {
     public void on_button_pushed() {
         if (state == ON) {
             System.out.println("반응 없음");
+        } else if(state == SLEEPING) {
+            System.out.println("Light On!");
         } else {
             System.out.println("Light On!");
             state = ON;
@@ -21,6 +24,9 @@ public class Light {
     public void off_button_pushed() {
         if(state == OFF) {
             System.out.println("반응 없음");
+        } else if (state == SLEEPING) {
+            System.out.println("Light Off!");
+            state = OFF;
         } else {
             System.out.println("Light Off!");
             state = OFF;
