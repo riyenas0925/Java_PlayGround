1bba740bee1ba00215f7fa15ddc1e04bdbbe35b7
riyenas0925 <riyenas0925@gmail.com>
2021-05-30 21:11:29 +0900

    feat(nextstep) : Calculator 단위테스트 연습

---
+++ src/main/java/nextstep/Calculator.java
@@ -0,0 +1,20 @@
+package nextstep;
+
+public class Calculator {
+
+    public int add(int i, int j) {
+        return i + j;
+    }
+
+    public int subtract(int i, int j) {
+        return i - j;
+    }
+
+    public int multiply(int i, int j) {
+        return i * j;
+    }
+
+    public int divide(int i, int j) {
+        return i / j;
+    }
+}
