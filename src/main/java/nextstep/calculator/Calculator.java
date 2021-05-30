68e1e9abef3937d4be1ee195bf69b188f8977520
riyenas0925 <riyenas0925@gmail.com>
2021-05-31 01:04:16 +0900

    chore(nextstep) :  calculator 패키지명 변경

--- src/main/java/nextstep/calculator/Calculator.java
+++ src/main/java/nextstep/calculator/Calculator.java
@@ -1,4 +1,4 @@
-package nextstep;
+package nextstep.calculator;
 
 public class Calculator {
 
--- src/test/java/nextstep/CalculatorTest.java
+++ src/test/java/nextstep/CalculatorTest.java
@@ -1,5 +1,6 @@
 package nextstep;
 
+import nextstep.calculator.Calculator;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
